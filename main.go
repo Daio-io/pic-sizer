db19412048d6bf20aed1621310cd4caa1219b002
Dai <dai.williams@bbc.co.uk>
2015-08-18 21:00:08 +0100

    Mess around script to resize an image on the internet

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "github.com/disintegration/imaging"
+import (
+	"github.com/disintegration/imaging"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
+	r := gin.Default()
+	r.GET("/image", func(c *gin.Context) {
 
+		file, err := os.Create("img.png")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		resp, err := http.Get(c.Query("img"))
+		if err != nil {
+			panic(err)
+		}
+		io.Copy(file, resp.Body)
+		size, err := strconv.ParseInt(c.Query("size"), 10, 0)
+		img, err := imaging.Open("./" + file.Name())
+		dstimg := imaging.Resize(img, int(size), 0, imaging.Box)
+		imaging.Save(dstimg, "img.resized.png")
+		c.File("./img.resized.png")
+	})
+
+	r.Run(":8080")
 }
